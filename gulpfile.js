28c1ea2d0b98eb022cf009f27a2c01b0dc34d8b1
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-05-19 10:52:55 +0200

    Gulp can clean project

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,28 @@
+/* global require, __dirname */
+
 'use strict';
 
 var gulp = require('gulp');
 var karma = require('karma').server;
+var del = require('del');
 
-gulp.task('test', function (done) {
-  karma.start({
-    configFile: __dirname + '/test/karma-conf-dev.js',
-    singleRun: true
-  }, done);
+gulp.task('test', function (cb) {
+    karma.start({
+        configFile: __dirname + '/test/karma-conf-dev.js',
+        singleRun: true
+    }, cb);
+});
+
+gulp.task('clean', function (cb) {
+    del([
+        'src/deps.js',
+        'src/style/app.css',
+        'src/TemplateCacheModule.js'
+    ], cb);
+});
+
+gulp.task('cleanall', function (cb) {
+    del([
+        'node_modules'
+    ]);
 });
