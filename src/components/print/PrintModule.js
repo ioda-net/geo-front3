d251e9c71603fe4757b83f4c88753a81593e37cd
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-06-16 16:35:41 +0200

    Correct require for ngeo

--- src/components/print/PrintModule.js
+++ src/components/print/PrintModule.js
@@ -3,14 +3,14 @@
 
   goog.require('ga_print_directive');
   goog.require('ga_print_style_service');
-  goog.require('ngeo_create_print');
-  goog.require('ngeo_print_utils');
+  goog.require('ngeo.CreatePrint');
+  goog.require('ngeo.PrintUtils');
 
   angular.module('ga_print', [
     'ga_print_directive',
     'ga_print_style_service',
-    'ngeo_create_print',
-    'ngeo_print_utils'
+    'ngeo.CreatePrint',
+    'ngeo.PrintUtils'
   ]);
 })();
 
