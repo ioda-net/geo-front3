bf3a5f7e2cbac1adf74f93de21952f17f9461dfc
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-07-17 12:30:08 +0200

    Fix tests for background selector

--- test/specs/backgroundselector/BackgroundSelectorDirective.spec.js
+++ test/specs/backgroundselector/BackgroundSelectorDirective.spec.js
@@ -18,12 +18,12 @@ describe('ga_backgroundselector_directive', function() {
         getOlLayerById: function(id) {
           return id == 'foo' ? layer1 : layer2;
         },
-        getBackgroundLayers: function() {
-          return [{
-            id: 'foo', label: 'Foo'
-          }, {
-            id: 'bar', label: 'Bar'
-          }];
+        getBackgroundLayers: function () {
+          return [
+            {id: 'ch.swisstopo.swissimage', label: 'bg_luftbild'},
+            {id: 'ch.swisstopo.pixelkarte-farbe', label: 'bg_pixel_color'},
+            {id: 'ch.swisstopo.pixelkarte-grau', label: 'bg_pixel_grey'}
+          ];
         }
       });
     });
@@ -41,6 +41,7 @@ describe('ga_backgroundselector_directive', function() {
       $compile(element)($rootScope);
       $rootScope.$digest();
 
+      $rootScope.$broadcast('gaLayersLoaded');
       $rootScope.$broadcast('gaLayersChange', {labelsOnly: false});
       $rootScope.$digest();
       rootScope = $rootScope;
@@ -54,6 +55,7 @@ describe('ga_backgroundselector_directive', function() {
       var div = divToggle[0];
       expect(div).not.to.be(undefined);
     });
+
     it('creates 4 layer bgselectors div', function() {
       var divsBg = element.find('.ga-bg-layer');
       expect(divsBg.length).to.equal(4);
