3d3bcf9f662d7412b1b1817cacd39974e22632a3
Julien Enselme <julien.enselme@ioda-net.ch>
2015-09-30 17:33:57 +0200

    Correct print integration test

--- test/selenium/print_test.js
+++ test/selenium/print_test.js
@@ -1,13 +1,15 @@
 describe('print', function() {
   it('prints', function() {
+    var printSubmit;
     // Click on "Drucken"
     $("#printHeading").click().then(function() {
       // Wait for mapfish print capabilities
       return browser.sleep(5000);
     }).then(function() {
-      return $('#print button[type*="submit"]').click();
+      printSubmit = $('#print button[type*="submit"]');
+      return printSubmit.click();
     }).then(function() {
-      return $('#print button[type*="submit"]').isDisplayed();
+      return printSubmit.isDisplayed();
     }).then(function(printButtonDisplayed) {
       expect(printButtonDisplayed).toBe(true);
 
