10b35185ead8efe54e02ff3d89f38bdc861d65d6
Julien Enselme <julien.enselme@ioda-net.ch>
2016-06-29 17:50:23 +0200

    Remove customer plugins

--- src/plugins/communes.js
+++
@@ -1,9 +0,0 @@
-function communes(mapCenter) {
-  var commune;
-  return $http.get(gaGlobalOptions.apiUrl + '/communes', {
-    params: {
-      x: mapCenter[0],
-      y: mapCenter[1]
-    }
-  });
-};
