0e5da7e8c21506b2ae55c5f5d1ebdaed1b637a78
Julien Enselme <julien.enselme@ioda-net.ch>
2016-08-25 17:35:56 +0200

    Correct comment in src/Gf3Plugins.nunjucks.js

--- src/Gf3Plugins.nunjucks.js
+++ src/Gf3Plugins.nunjucks.js
@@ -1,7 +1,5 @@
 {#
   This is a generated file which will contain the code for all activated plugins.
-  If a plugin is available but not activated, it is replaced by a function that
-  returns undefined. This way, the call to a not activated plugin will not fail.
 #}
 // We use gaGlobalOptions but we don't explicitely require
 // goog.require('ga'); because closure-compiler will crash due to circular
