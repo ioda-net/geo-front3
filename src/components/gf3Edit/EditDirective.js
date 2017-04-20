9df81952b687645ce34af3ed75250760ad9739bf
Julien Enselme <julien.enselme@ioda-net.ch>
2017-04-20 10:56:05 +0200

    Rename is3D into hasZ in Open Layers

--- src/components/gf3Edit/EditDirective.js
+++ src/components/gf3Edit/EditDirective.js
@@ -302,7 +302,7 @@ goog.provide('gf3_edit_directive');
             featureType: scope.layer.featureType,
             srsName: scope.layer.srsName,
             featurePrefix: scope.layer.featurePrefix,
-            is3D: scope.layer.is3D,
+            hasZ: scope.layer.is3D,
             version: scope.layer.version
           };
           scope.message = $translate.instant('edit_saving');
